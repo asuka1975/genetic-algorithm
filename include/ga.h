ae9a4527498eda534ca3c9298598cedb12749be5
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-07-22 20:13:54 +0900

    determined genome requisitions

---
+++ include/ga.h
@@ -0,0 +1,14 @@
+//
+// Created by hungr on 2020/07/22.
+//
+
+#ifndef GENETIC_GA_H
+#define GENETIC_GA_H
+
+#include "genome.h"
+
+namespace genetic {
+
+}
+
+#endif //GENETIC_GA_H
---
+++ include/genome.h
@@ -0,0 +1,22 @@
+//
+// Created by hungr on 2020/07/22.
+//
+
+#ifndef GENETIC_GENOME_H
+#define GENETIC_GENOME_H
+
+#include <cstdint>
+#include <type_traits>
+
+namespace genetic {
+    inline namespace genetic_internal {
+        inline constexpr int crossover = 0;
+    }
+    template <class T, class = void>
+    struct is_genome : std::false_type<T> { };
+
+    template <class T, std::enable_if_t<std::is_copy_assignable_v<T> && std::is_invocable_r_v<T, decltype(crossover), T, T>>>
+    struct is_genome : std::true_type<T> { };
+}
+
+#endif //GENETIC_GENOME_H
