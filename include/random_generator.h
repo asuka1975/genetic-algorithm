d9d21570cc75a09b0c027eff7dadef280cda54f4
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-07-27 10:46:54 +0900

    changed timing of locking resource and changed access of thread_own_gens elements from operator[] to at()

--- include/random_generator.h
+++ include/random_generator.h
@@ -48,59 +48,59 @@ T random_generator::random() {
 
 template<class T, std::enable_if_t<std::is_integral_v<T>, std::nullptr_t>>
 T random_generator::random_uniform(T min, T max) {
-    std::lock_guard<decltype(mtx)> lock(mtx);
     std::uniform_int_distribution<T> dist(min, max);
     auto&& id = std::this_thread::get_id();
     if(thread_own_gens.find(id) == thread_own_gens.end()) {
+        std::lock_guard<decltype(mtx)> lock(mtx);
         thread_own_gens.emplace(id, std::mt19937_64{seed_gen()});
     }
-    return dist(thread_own_gens[id]);
+    return dist(thread_own_gens.at(id));
 }
 
 template<class T, std::enable_if_t<std::is_floating_point_v<T>, std::nullptr_t>>
 T random_generator::random_uniform(T min, T max) {
-    std::lock_guard<decltype(mtx)> lock(mtx);
     std::uniform_real_distribution<T> dist(min, max);
     auto&& id = std::this_thread::get_id();
     if(thread_own_gens.find(id) == thread_own_gens.end()) {
+        std::lock_guard<decltype(mtx)> lock(mtx);
         thread_own_gens.emplace(id, std::mt19937_64{seed_gen()});
     }
-    return dist(thread_own_gens[id]);
+    return dist(thread_own_gens.at(id));
 }
 
 template<class T, std::enable_if_t<std::is_integral_v<T> && std::is_signed_v<T>, std::nullptr_t>>
 T random_generator::random_normal(T mean, T max) {
-    std::lock_guard<decltype(mtx)> lock(mtx);
     if(auto&& m = std::numeric_limits<T>::max() / 2; max >= m) max = m;
     std::binomial_distribution<T> dist(max * 2, 0.5);
     auto&& id = std::this_thread::get_id();
     if(thread_own_gens.find(id) == thread_own_gens.end()) {
+        std::lock_guard<decltype(mtx)> lock(mtx);
         thread_own_gens.emplace(id, std::mt19937_64{seed_gen()});
     }
-    return dist(thread_own_gens[id]) - max + mean;
+    return dist(thread_own_gens.at(id)) - max + mean;
 }
 
 template<class T, std::enable_if_t<std::is_integral_v<T> && std::is_unsigned_v<T>, std::nullptr_t>>
 T random_generator::random_normal(T mean, T max) {
-    std::lock_guard<decltype(mtx)> lock(mtx);
     if(auto&& m = std::numeric_limits<T>::max() / 2; max >= m) max = m;
     std::binomial_distribution<T> dist(max * 2, 0.5);
     auto&& id = std::this_thread::get_id();
     if(thread_own_gens.find(id) == thread_own_gens.end()) {
+        std::lock_guard<decltype(mtx)> lock(mtx);
         thread_own_gens.emplace(id, std::mt19937_64{seed_gen()});
     }
-    return dist(thread_own_gens[id]) - max + mean;
+    return dist(thread_own_gens.at(id)) - max + mean;
 }
 
 template<class T, std::enable_if_t<std::is_floating_point_v<T>, std::nullptr_t>>
 T random_generator::random_normal(T mean, T stddev) {
-    std::lock_guard<decltype(mtx)> lock(mtx);
     std::normal_distribution<T> dist(mean, stddev);
     auto&& id = std::this_thread::get_id();
     if(thread_own_gens.find(id) == thread_own_gens.end()) {
+        std::lock_guard<decltype(mtx)> lock(mtx);
         thread_own_gens.emplace(id, std::mt19937_64{seed_gen()});
     }
-    return dist(thread_own_gens[id]);
+    return dist(thread_own_gens.at(id));
 }
 
 #endif //GENETIC_RANDOM_GENERATOR_H
