9e03841ceded1abde843ff7983041f15745116f1
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-11-04 13:56:55 +0900

    In random_generator, change mt19937 to thread local variable

--- src/random_generator.cpp
+++ src/random_generator.cpp
@@ -4,5 +4,4 @@
 #include "random_generator.h"
 
 std::random_device random_generator::seed_gen;
-std::map<std::thread::id, std::mt19937_64> random_generator::thread_own_gens;
-std::mutex random_generator::mtx;
+thread_local std::mt19937_64 random_generator::mt(random_generator::seed_gen());
